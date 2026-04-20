4d3c581087f6198e01a0636c2e6ff5c5b6ca0d96
Ngina-Misha Awan <mawan@macalester.edu>
2026-04-20 10:17:27 -0500

    started

---
+++ src/MineGame.java
@@ -0,0 +1,24 @@
+import edu.macalester.graphics.CanvasWindow;
+
+public class MineGame {
+    private CanvasWindow canvas;
+    private final int CANVAS_WIDTH = 400;
+    private final int CANVAS_HEIGHT = 400;
+
+    public MineGame() {
+        this.canvas = new CanvasWindow("Mine Game", CANVAS_WIDTH, CANVAS_HEIGHT);
+
+        
+    }
+
+    public void runGame() {
+    }
+
+
+
+
+    public static void main(String[] args) {
+        new MineGame();
+    }
+    
+}
---
+++ src/Rectangle.java
@@ -0,0 +1,13 @@
+import edu.macalester.graphics.CanvasWindow;
+
+public class Rectangle {
+    private double width;
+    private double height;
+    private CanvasWindow canvas;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+}
